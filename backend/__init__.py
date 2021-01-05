ce43bd3af8f85fe8b5108a7a1241cad49cf5e85f
NoviceMaster <mrchenbin@126com>
2021-01-05 17:59:31 +0800

    项目立项

---
+++ backend/__init__.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : NoviceMaster
+# @Email   : mrchenbin@126.com
+# @Time    : 2021/1/5 17:50 
+# @File    : __init__.py.py
+# @Software: PyCharm
